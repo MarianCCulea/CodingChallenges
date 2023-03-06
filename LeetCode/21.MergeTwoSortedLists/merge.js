4d4f99410fd71efb9e66f667730fcebd42f570d6
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2023-03-06 22:15:58 +0100

    Solution 21 and 26

---
+++ LeetCode/21.MergeTwoSortedLists/merge.js
@@ -0,0 +1,42 @@
+//Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode();
+
+  if (!list1 || !list2) return list2 || list1;
+
+  if (list1.val < list2.val) {
+    head = list1;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    list2 = list2.next;
+  }
+
+  // new pointer
+  let current = head;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  let remaining = list1 || list2;
+  current.next = remaining;
+  return head;
+};
+
+module.exports = { mergeTwoLists };
